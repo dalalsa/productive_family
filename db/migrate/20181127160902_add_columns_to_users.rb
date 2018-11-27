9deaab96eebedf12e4c7b052c760e1cf945c14ba
thekraalowais <thekra_alowais@hotmail.com>
2018-11-27 19:21:51 +0300

    Create product, user modules

---
+++ db/migrate/20181127160902_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :location, :string
+    add_index :users, :username, unique: true
+
+  end
+end
