ebcb52976dbaa0cc0dc32f405acdfcd64efb7be7
dalalsa <sarebdesign@gmail.com>
2018-11-29 10:39:27 +0300

    Products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,32 @@
+class ProductsController < ApplicationController
+  def index
+
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create 
+    product = Product.create(product_params)
+   # redirect_to product_path(product)
+
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+   private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :image, :category, :price ,:seller_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+ 
   devise_for :sellers
   devise_for :users
+   resources :products
 root "pages#home"
 end
