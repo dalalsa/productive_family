7296285a79f279b45a85910bcf942c1c2e3c6795
thekraalowais <thekra_alowais@hotmail.com>
2018-11-30 02:13:41 +0300

    Logic without item and order controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-User.destroy_all
-Product.destroy_all 
-Item.destroy_all 
-Cart.destroy_all
-Order.destroy_all 
-Seller.destroy_all
+# User.destroy_all
+# Product.destroy_all 
+# Item.destroy_all 
+# Cart.destroy_all
+# Order.destroy_all 
+# Seller.destroy_all
+
+# Product.create(name: "cakes store", image:"ddddd", price: 22, category: "sweet", description: "cakes for best taste", seller_id: 2 );Product.create(name: "cakes store", image:"ddddd", price: 22, category: "sweet", description: "cakes for best taste", seller_id: 3 );
