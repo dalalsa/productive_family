d3634d3869fd115dcd6e4a987faabdb234643e05
thekraalowais <thekra_alowais@hotmail.com>
2018-11-29 10:48:58 +0300

    Order controller uncomplete

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,10 @@ Rails.application.routes.draw do
  
   devise_for :sellers
   devise_for :users
-<<<<<<< HEAD
-root "pages#home"
-resources :orders do 
-resources :items
-
-end
-=======
   resources :sellers, only: [:index, :show]
   # index sellers will be in the home page  
   # Show seller when a customer choose the seller (add to cart and products will be there) /Nora
   resources :users, only: [:index, :show]
   # in my opinion we need index and show users JUST for us as an admin / Nora
   root "pages#home"
->>>>>>> 7b3a8ca41e79e3fd6863ddc2e9a788d0dd4126d8
 end
