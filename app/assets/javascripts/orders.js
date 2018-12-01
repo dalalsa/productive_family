9e96810de85de7ae4fb6681d275a7d4f7926c96e
thekraalowais <thekra_alowais@hotmail.com>
2018-12-01 21:34:01 +0300

    Google map uncompleted

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,2 +1,43 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// // Place all the behaviors and hooks related to the matching controller here.
+// // All this logic will automatically be available in application.js.
+
+// function getAddress() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       //   console.log(position);
+//       let latResult = position.coords.latitude;
+//       let longResult = position.coords.longitude;
+//       console.log("lat " + latResult, "long " + longResult);
+
+//     });
+//   } else {
+//     console.log("geolocation is not supported ");
+//   }
+// }
+
+// window.onload = function() {
+//   const form = document.querySelector("form");
+//   form.addEventListener("submit", e => {
+//     e.preventDefault();
+//     // console.log("clicked");
+//     getAddress();
+//   });
+// };
+
+// function initMap() {
+//   var myLatLng = { lat: -25.363, lng: 131.044 };
+
+//   // Create a map object and specify the DOM element
+//   // for display.
+//   var map = new google.maps.Map(document.getElementById("map"), {
+//     center: myLatLng,
+//     zoom: 4
+//   });
+
+//   // Create a marker and set its position.
+//   var marker = new google.maps.Marker({
+//     map: map,
+//     position: myLatLng,
+//     title: "Hello World!"
+//   });
+// }
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,23 +8,23 @@ class ItemsController < ApplicationController
   chosen_product = Product.find(params[:product_id])
   current_cart = @current_cart
 
-  if current_cart.products.include?(chosen_product)
-    @item = current_cart.items.find_by(:product_id => chosen_product)
-    @item.quantity += 1
-  else
-    # @item = Item.new(item_params)
-    # @item.cart = current_cart
-    # @item.product = chosen_product
+  # if current_cart.products.include?(chosen_product)
+  #   @item = current_cart.items.find_by(:product_id => chosen_product)
+  #   @item.quantity += 1
+  # else
+  #   @item = Item.new(item_params)
+  #   @item.cart = current_cart
+  #   @item.product = chosen_product
     # current_cart = 
-  end
-  @item.save
+  # end
+  # @item.save
   
   redirect_to cart_path(current_cart)
 
 
 end
-private
-  def item_params
-    params.require(:item).permit(:product_id, :quantity,:cart_id, :order_id)
-  end
+# private
+#   def item_params
+#     params.require(:item).permit(:product_id, :quantity,:cart_id, :order_id)
+#   end
 end
