7643a0634403da7924796dbe5e3b99df2746b603
dalalsa <sarebdesign@gmail.com>
2018-12-02 17:11:56 +0300

    order status

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
-class OrdersController < ApplicationController#   def index
-#     @orders=Order.all
-#   end
-
+class OrdersController < ApplicationController #   def index
+  #     @orders=Order.all
+  #   end
   def index
-    @orders = Order.all
+    @orders = Order.where(user_id: current_user)
+    # @orders = Order.all
   end
 
   def show
@@ -12,22 +12,37 @@ class OrdersController < ApplicationController#   def index
 
   def new
     @order = Order.new
+    @cart = @current_cart
   end
-  # raise "kj"
-  def create
-  @order = Order.new(order_params)
-  @current_cart.items.each do |item|
-    @order.items << item
-    item.cart_id = nil
-  end
-  @order.save
-  Cart.destroy(session[:cart_id])
-  session[:cart_id] = nil
-  redirect_to root_path
-end
 
-private
-  def order_params
-    params.require(:order).permit(:user_id, :status)
+  def approve
+    order = Order.find_by(id: params[:id])
+    order.status = "Approved"
+    order.save
   end
+
+  def reject
+    order = Order.find_by(id: params[:id])
+    order.status = "Rejected"
+    order.save
+  end
+
+  def create
+    order = current_user.orders.create(status: "pending")
+
+    # @current_cart.items.each do |item|
+    #   @order.items << item
+    #   item.cart_id = nil
+    # end
+
+    # Cart.destroy(session[:cart_id])
+    # session[:cart_id] = nil
+    # redirect_to root_path
+  end
+
+  # private
+
+  # def order_params
+  #   params.require(:order).permit(:user_id, :status => "p")
+  # end
 end
--- db/migrate/20181202073716_add_default_zero_to_items.rb
+++ db/migrate/20181202073716_add_default_zero_to_items.rb
@@ -1,5 +1,5 @@
 class AddDefaultZeroToItems < ActiveRecord::Migration[5.2]
   def change
-    # change_column_default :items, :quantity, 0
+    change_column_default :items, :quantity, 0
   end
 end
