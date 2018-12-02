94571d42f7b03e3674b9cc51735919f3d6f0a4a1
NouraFL <nour3_@hotmail.com>
2018-12-02 19:59:19 +0300

    save changes after congig fexed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_085850) do
+ActiveRecord::Schema.define(version: 2018_12_02_073716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
