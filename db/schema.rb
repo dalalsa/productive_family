efb4f3e0e0cf3686334c2e2a608bf1be205d97dc
thekraalowais <thekra_alowais@hotmail.com>
2018-12-03 10:31:41 +0300

    add columns to user and item default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_085850) do
+ActiveRecord::Schema.define(version: 2018_12_03_070243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_12_02_085850) do
   create_table "items", force: :cascade do |t|
     t.integer "order_id"
     t.integer "product_id"
-    t.integer "quantity", default: 0
+    t.integer "quantity", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "cart_id"
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 2018_12_02_085850) do
     t.string "username"
     t.string "address"
     t.string "full_name"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
